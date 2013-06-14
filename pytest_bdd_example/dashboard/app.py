f33520628cc4c05085129f50afcbb278fc5509f6
Oleg Podsadny <oleg.podsadny@gmail.com>
2013-06-15 00:34:16 +0200

    before merge

--- pytest_bdd_example/dashboard/app.py
+++ pytest_bdd_example/dashboard/app.py
@@ -1,12 +1,12 @@
-import os
 from flask import Flask
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqlamodel import ModelView
+from flask.ext.sqlalchemy import SQLAlchemy
 
 from pytest_bdd_example.dashboard import settings
 
 from pytest_bdd_example.auth import auth
-from pytest_bdd_example.book import book, Author, Book, db as book_db
+from pytest_bdd_example.book import book, Book
 
 
 app = Flask(
@@ -17,8 +17,10 @@ app = Flask(
 app.config.from_object('pytest_bdd_example.dashboard.settings')
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
+db = SQLAlchemy(app)
 
 app.register_blueprint(auth, url_prefix='/auth')
+app.register_blueprint(book)
 
 admin = Admin(app)
-admin.add_view(ModelView(Book, book_db.Session(), 'books', endpoint='books'))
+admin.add_view(ModelView(Book, db.Session(), 'books', endpoint='books'))
