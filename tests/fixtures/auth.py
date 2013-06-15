135bbeb6ba07c97f85340bef8755ab4d6ce27a32
Andrey Makhnach <A.Makhnach@modnakasta.ua>
2013-06-15 23:38:27 +0300

    fixed imports in tests

--- tests/fixtures/auth.py
+++ tests/fixtures/auth.py
@@ -1,7 +1,7 @@
 import pytest
 
-from pytest_bdd_example.dashboard.models import User
-from pytest_bdd_example.dashboard import db
+from pytest_bdd_example.auth.models import User
+from pytest_bdd_example.dashboard.app import db
 
 from tests.helpers.random import random_string
 
@@ -14,7 +14,8 @@ def password():
 @pytest.fixture
 def user(password):
     u = User(
-        username=u'{0}@example.com'.format(random_string(7)),
+        username=u'{0}'.format(random_string(7)),
+        email="{0}@example.com".format(random_string(7)),
         password=password,
     )
     db.session.add(u)
