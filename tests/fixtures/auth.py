198e1ff663c23b5b09e8b403a6f8facf64d6a12f
Oleg Podsadny <oleg.podsadny@gmail.com>
2013-06-15 22:53:10 +0200

    fixes import

--- tests/fixtures/auth.py
+++ tests/fixtures/auth.py
@@ -1,7 +1,6 @@
 import pytest
 
 from pytest_bdd_example.auth.models import User
-from pytest_bdd_example.dashboard.app import db
 
 from tests.helpers.random import random_string
 
@@ -13,11 +12,12 @@ def password():
 
 @pytest.fixture
 def user(password):
+    session = User.query.session
     u = User(
         username=u'{0}'.format(random_string(7)),
         email="{0}@example.com".format(random_string(7)),
         password=password,
     )
-    db.session.add(u)
-    db.session.commit()
+    session.add(u)
+    session.commit()
     return u
---
+++ tests/functional/dashboard/conftest.py
@@ -0,0 +1,5 @@
+from pytest_bdd_example.dashboard import app
+
+with app.app_context():
+    """Initialize the application context."""
+    pass
